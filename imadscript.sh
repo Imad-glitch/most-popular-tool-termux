f4467c3e3251c4c0ab2af464a44149e2dd218a96
imadtouki <78638844+Imad-glitch@users.noreply.github.com>
2021-08-31 18:28:01 +0100

    Create imadscript.sh

---
+++ imadscript.sh
@@ -0,0 +1,29 @@
+apt update -y
+apt upgrade -y
+apt install toilet -y
+apt install lolcat -y
+toilet imad |lolcat
+toilet hacker|lolcat
+toilet script |lolcat
+echo  "start of download please wait ..."
+sleep 1
+echo "start now"
+sleep 2
+toilet programme |lolcat
+toilet started |lolcat
+echo "#################################"
+apt install git -y
+apt install python -y
+apt install python2 -y
+apt install figlet -y
+apt install toilet -y
+apt install irssi -y
+apt install neofetch -y
+apt install cmatrix -y
+sleep 1
+clear
+neofetch
+figlet programme |lolcat
+figlet finiched |lolcat
+sleep 4
+clear
